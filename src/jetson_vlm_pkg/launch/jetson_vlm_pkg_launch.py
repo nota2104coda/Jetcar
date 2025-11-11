c86c4de9a4e78b5fdf646b9617d6b181c53b22d8
nota2104coda <jeevanghadge@gmail.com>
2025-11-11 21:27:13 +0000

    added submodule for jetson containers

---
+++ src/jetson_vlm_pkg/launch/jetson_vlm_pkg_launch.py
@@ -0,0 +1,76 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    # Get the package directory
+    pkg_share_dir = get_package_share_directory('pi5_pkg')
+    venv_path = os.path.expanduser('~/PicoWCar/.venv/lib/python3.12/site-packages')
+    
+    # Declare a launch argument for the robot model file
+    robot_model_path_arg = DeclareLaunchArgument(
+        'robot_model',
+        default_value=os.path.join(pkg_share_dir, 'urdf', 'skid_steer_4wd.urdf'),
+        description='Path to the robot URDF/xacro file'
+    )
+    
+    # Load the robot description from the URDF file
+    robot_description = Command(['xacro ', LaunchConfiguration('robot_model')])
+
+    # Node to publish the robot's state
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description}]
+    )
+
+    # Your custom Python nodes
+    camera_node = Node(
+        package='pi5_pkg',
+        executable='camera_node',
+        name='camera',
+        output='screen'
+    )
+    hmi_node = Node(
+        package='pi5_pkg',
+        executable='hmi_node',
+        name='hmi_node',
+        output='screen',
+        env={
+                'PYTHONPATH': f"{venv_path}:{os.environ.get('PYTHONPATH', '')}"
+            }
+    )
+    # # smolvla_node = Node(
+    #     package='pi5_pkg',
+    #     executable='smolvla_node.py',
+    #     name='vla_decision_node',
+    #     output='screen'
+    # )
+    
+    # motor_control_node = Node(
+    #     package='pi5_pkg',
+    #     executable='motor_control_node.py',
+    #     name='motor_control_node',
+    #     output='screen'
+    # )
+    
+    # robot_state_node = Node(
+    #     package='pi5_pkg',
+    #     executable='robot_state_node.py',
+    #     name='robot_state_node',
+    #     output='screen'
+    # )
+
+    # Launch description with all the nodes
+    return LaunchDescription([
+        robot_model_path_arg,
+        robot_state_publisher_node,
+        hmi_node,
+        camera_node,
+        # smolvla_node,
+    ])
