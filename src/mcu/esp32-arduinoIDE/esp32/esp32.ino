eb6aab4ef904417391598ee6601ea059bbba84a4
nota2104coda <jeevanghadge@gmail.com>
2026-01-23 02:15:08 +0000

    YAAY - ESP 32 debugging now works!

--- src/mcu/esp32-arduinoIDE/esp32/esp32.ino
+++ src/mcu/esp32-arduinoIDE/esp32/esp32.ino
@@ -4,11 +4,13 @@ constexpr gpio_num_t LED_PIN = GPIO_NUM_47;  // On-board RGB LED blue channel on
 
 void setup() {
     pinMode(LED_PIN, OUTPUT);
+    Serial.begin(115200);
 }
 
 void loop() {
     digitalWrite(LED_PIN, HIGH);
     delay(500);
+    Serial.println("hello");
     digitalWrite(LED_PIN, LOW);
     delay(500);
 }
