3be571d481edbb2810e591f57a71263379c38d42
nota2104coda <jeevanghadge@gmail.com>
2026-05-27 00:21:01 +0100

    feat: import TimerAction in gazebo launch configuration

--- src/jetcar_sim/launch/gazebo.launch.py
+++ src/jetcar_sim/launch/gazebo.launch.py
@@ -2,7 +2,7 @@ import os
 from launch.substitutions import Command
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, RegisterEventHandler
+from launch.actions import IncludeLaunchDescription, RegisterEventHandler , TimerAction
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
